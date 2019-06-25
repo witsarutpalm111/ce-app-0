d8180427c99bf6014d9d2cd83150464be79588b5
HAlice <37956546+MidnightCore@users.noreply.github.com>
2019-06-25 14:42:23 +0700

    fix

--- user/add_user/FormInsert_user2.php
+++ user/add_user/FormInsert_user2.php
@@ -37,9 +37,9 @@
         }
     </style>
 
-    <style type="text/css">/* เอาscrollup-down ออก */
+    <!-- <style type="text/css">/* เอาscrollup-down ออก */
         html, body { overflow:  hidden; }       
-    </style>
+    </style> -->
 </head>
 
 
