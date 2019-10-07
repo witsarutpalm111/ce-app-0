4398d26ae57756188995539688e05d3452bcaf69
witsarutpalm111 <47667421+witsarutpalm111@users.noreply.github.com>
2019-10-07 21:43:34 +0700

    ....

--- DB_ce-app/User.php
+++ DB_ce-app/User.php
@@ -4,6 +4,8 @@ $hostname = "localhost";
 $user = "root";
 $pass ="";
 $dbName ="id11140592_cessrufixshop";
+$dbName ="ce-app";
+
 $connect = mysqli_connect($hostname,$user,$pass,$dbName) or die ("ไม่สามารถติดต่อฐานข้อมูลได้ นะจ้ะ");
 mysqli_set_charset($connect,"utf8");
 ?>
