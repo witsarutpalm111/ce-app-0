a18f016702d969bbeaba74305b6dfe331c2000dd
[Alice] <[setsuna8741@gmail.com]>
2019-10-13 09:57:15 +0700

    .

--- DB_ce-app/User.php
+++ DB_ce-app/User.php
@@ -3,7 +3,7 @@
 $hostname = "localhost";
 $user = "root";
 $pass ="";
-$dbName ="id11140592_cessrufixshop";
+$dbName ="ce-app-0";
 $connect = mysqli_connect($hostname,$user,$pass,$dbName) or die ("ไม่สามารถติดต่อฐานข้อมูลได้ นะจ้ะ");
 mysqli_set_charset($connect,"utf8");
 ?>
