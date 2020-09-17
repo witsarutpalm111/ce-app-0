94104a32b1a5f65fa8340a7e4fd5700513794665
witsarutpalm111 <47667421+witsarutpalm111@users.noreply.github.com>
2020-09-17 11:40:38 +0700

    name datebase

--- DB_ce-app/User.php
+++ DB_ce-app/User.php
@@ -3,7 +3,7 @@
 $hostname = "localhost";
 $user = "root";
 $pass ="";
-$dbName ="ce-app-0";
+$dbName ="ce-app";
 $connect = mysqli_connect($hostname,$user,$pass,$dbName) or die ("ไม่สามารถติดต่อฐานข้อมูลได้ นะจ้ะ");
 mysqli_set_charset($connect,"utf8");
 ?>
